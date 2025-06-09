7a768502514e2bd849a8f48a595d038312de2049
Nishant Singh <nishantsingh2n@gmail.com>
2025-06-09 12:51:53 +0530

    update

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
-const API_URL = "https://taskmanager-xfsz.onrender.com/api";
+ const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
+//const API_URL = "https://taskmanager-xfsz.onrender.com/api";
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URL });
 
