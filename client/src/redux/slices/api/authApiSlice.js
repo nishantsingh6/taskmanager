a06871021456194c17f1e8458b6ce485e2a71520
Nishant Singh <nishantsingh2n@gmail.com>
2025-06-09 13:08:30 +0530

    auth

--- client/src/redux/slices/api/authApiSlice.js
+++ client/src/redux/slices/api/authApiSlice.js
@@ -8,7 +8,6 @@ export const authApiSlice = apiSlice.injectEndpoints({
         url: `${USERS_URL}/login`,
         method: "POST",
         body: data,
-        credentials: "include",
       }),
     }),
     register: builder.mutation({
@@ -16,14 +15,12 @@ export const authApiSlice = apiSlice.injectEndpoints({
         url: `${USERS_URL}/register`,
         method: "POST",
         body: data,
-        credentials: "include",
       }),
     }),
     logout: builder.mutation({
       query: () => ({
         url: `${USERS_URL}/logout`,
         method: "POST",
-        credentials: "include",
       }),
     }),
   }),
